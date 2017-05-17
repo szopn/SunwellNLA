e55b6b4126cc492a3e5bb8b3d90f492e1ad76cdd
szopn <szopn@o2.pl>
2017-05-17 20:03:00 +0200

    poprawka

--- NLA.lua
+++ NLA.lua
@@ -3,6 +3,7 @@
 addonnotification=true
 addonemote=true
 soundalert=true 
+devmode=false
 testnickname="Cap" -- example nickname from .listninja
 -- CONFIG --
 
@@ -24,23 +25,40 @@ testnickname="Cap" -- example nickname from .listninja
 		end	
 		if(event == "CHAT_MSG_SYSTEM")
 		then
-
+			--ChatFrame_AddMessageEventFilter("CHAT_MSG_SYSTEM",true) --disable sunwell notification
 		   local system_msg = select(1, ...)
 		    if (system_msg:find("Warning! Player:.") ~= nil) then
-				local uwagazlodziej=0
+				local robbers = {}
+				local attentionrobbers=0
+				sub_system_msg = strsub (system_msg, 17)
+				robbers[1], robbers[2] = sub_system_msg:match("(%w+)(.+)")
+				--print(robbers[1])
+				--print("NumRaidMembers "..GetNumRaidMembers())
 				for i=1,GetNumRaidMembers() do
 					local u="raid"..i 
+					raidlist[u]=UnitName(u)
+					--DEBUG START--
 					if(devmode==true)
 					then
+						raidlist[u]=testnickname
 					end					
+					--DEBUG END----
+					if(raidlist[u]==robbers[1])
 					then
+						attentionrobbers=robbers[1]			
 					else
+						attentionrobbers=0
 					end
-
 				end				
+				if(attentionrobbers~=0)
 				then
+					if(addonnotification==true)then UIErrorsFrame:AddMessage("Ninjalooter in your party: " ..attentionrobbers)	end
+					if(addonemote==true)then SendChatMessage("sees ninjalooter in your party: " ..attentionrobbers, "EMOTE", DEFAULT_CHAT_FRAME.editBox.languageID) end			
+					if(soundalert==true)then PlaySound("AuctionWindowOpen")	end
+					attentionrobbers=0			
 				end
 			else	
+				--ChatFrame_RemoveMessageEventFilter("CHAT_MSG_SYSTEM", true)	-- enable sunwell notification					
 		    end
 	    end	
 	end
